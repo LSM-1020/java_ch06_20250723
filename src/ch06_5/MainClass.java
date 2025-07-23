5a5f8858bc00a8e46dbb0b679ebcc86594f3b623
LSM-1020 <smmlee@naver.com>
2025-07-23 17:03:31 +0900

    스태틱

---
+++ src/ch06_5/MainClass.java
@@ -0,0 +1,19 @@
+package ch06_5;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticTest st1 = new StaticTest(); //인스턴스(객체)생성
+		st1.width = 10;
+		st1.height = 20;
+		st1.color = "red";
+		
+		StaticTest st2 = new StaticTest(); //인스턴스(객체)생성
+		st2.width = 50;
+		st2.height = 60;
+		st2.color = "blue";
+		
+	}
+
+}
---
+++ src/ch06_5/StaticTest.java
@@ -0,0 +1,10 @@
+package ch06_5;
+
+public class StaticTest {
+	static final double PI = 3.14; //정적 멤버(static 멤버), 정적 변수 선언
+	int width;
+	int height;
+	String color;
+}
+//Pi같은 고정값은 Static으로 선언하여 공유하여 쓸수있도록 설정함
+//하나의 치약을 가족들이 같이쓰는 느낌 , final을 붙이면 값을 바꿀수없음
