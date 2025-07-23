eb38894c6ecb7426c494da113c6feb6e552898d0
LSM-1020 <smmlee@naver.com>
2025-07-23 15:39:28 +0900

    책290p 연습문제 추가

---
+++ src/ch06_4/Printer.java
@@ -0,0 +1,18 @@
+package ch06_4;
+
+public class Printer {
+
+	public void println(int val) {
+		System.out.println(val);
+	}
+	public void println(boolean val) {
+		System.out.println(val);
+	}
+	public void println(double val) {
+		System.out.println(val);
+	}
+	public void println(String val) {
+		System.out.println(val);
+	}
+}
+// int double처럼 여러가지를 만들어놨기에 같은 이름을써도 다양한 방식으로 나타낼수 있음
