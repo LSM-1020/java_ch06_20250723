ae362f6323997643f4badb41b7c9a958ec655ca6
LSM-1020 <smmlee@naver.com>
2025-07-23 12:49:57 +0900

    메소드 사용법

---
+++ src/ch06_4/Main.java
@@ -0,0 +1,16 @@
+package ch06_4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calculator cal1 = new Calculator();
+		int result = cal1.mul(20, 10);
+		System.out.println(result);
+		
+		
+		
+	}
+
+}
